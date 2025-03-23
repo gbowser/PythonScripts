5609d9fd5e3ca963f32ca394b8f038abd1b5a7ce
gordonbowser <gordon@bowser.net>
2025-03-23 11:02:46 +0000

    more updates

--- Data Driven Astronomy/test 2025-03-23.py
+++ Data Driven Astronomy/test 2025-03-23.py
@@ -1,2 +1,6 @@
 print("hello world\nHello World")
+print("Hello" + " " + "Gordon")
 
+myname = print ("Hello what is your name" +input( say again?"))
+print("Hello" + myname)
+                                                 
