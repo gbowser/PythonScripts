f2e79f3654be9e9351ccec8795d25f0d6e572709
gordonbowser <gordon@bowser.net>
2026-03-23 17:34:37 +0000

    Refactor data loading and parsing in numpy script for student data analysis

--- Scientific_Programming/Section 6.2 page 228 Reading and Writing Files in numpy.py
+++ Scientific_Programming/Section 6.2 page 228 Reading and Writing Files in numpy.py
@@ -3,6 +3,10 @@ import numpy as np
 
 fname = 'eg6-a-student-data.txt'
 dtype1 = np.dtype([('gender', '|S1'), ('height', 'f8')])
+dtype2 = np.dtype([('gender', '|S1'), ('weight', 'f8')])
+dtype3 = np.dtype([('gender', '|S1'), ('bps', 'f8'), ('bpd', 'f8')])
+
+
 a = np.loadtxt(fname, dtype=dtype1, skiprows=9, usecols=(1,3))
 
 print(f"{a}")
@@ -22,10 +26,30 @@ def parse_weight(a):
     except ValueError:
         return -99
 
-dtype2 = np.dtype([('gender', '|S1'), ('weight', 'f8')])
+def parse_bp(s):
+    try:
+        return float(s)
+    except ValueError:
+        return -99
+
+
 b = np.loadtxt(fname, dtype=dtype2, skiprows=9, usecols=(1,4), converters={4: parse_weight})
 
-mv=b['weight']>0
-m_wav=b['weight'][mv & m].mean()
-f_wav=b['weight'][mv & ~m].mean()
+m = b['gender'] == b'M'
+mv = b['weight'] > 0
+m_wav = b['weight'][mv & m].mean()
+f_wav = b['weight'][mv & ~m].mean()
 print(f"Male weight average: {m_wav:.2f} , Female weight average: {f_wav:.2f} ")
+
+
+def reformat_lines(fi):
+    for line in fi:
+        line=line.replace('/', ' ')
+        yield line
+
+with open(fname) as fi:
+    gender,bps, bpd=np.loadtxt(reformat_lines(fi), dtype3, skiprows=9, usecols=(1,7,8), converters={7:parse_bp,8:parse_bp}, unpack=True)
+
+#now do something with the data
+print(f"Gender = {gender}\nBPS = {bps}\nBPD = {bpd}")
+      
